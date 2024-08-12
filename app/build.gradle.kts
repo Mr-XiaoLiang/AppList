3c8e2c59cb2c55d5f46ddb94a75dd6106267d711
lollipop <liujian@diguotech.com>
2024-08-12 10:22:57 +0800

    feat: 更新 1.3.0 调整应用启动列表的展示方式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.lollipop.applist"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1_02_00
-        versionName = "1.2.0"
+        versionCode = 1_03_00
+        versionName = "1.3.0"
     }
 
     buildTypes {
